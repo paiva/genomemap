f47931e0c9f8fdfdba6518a36d278efef4b4039b
Santiago <santiago@semanttica.com>
2014-02-02 01:08:56 -0500

    added new files

---
+++ streaming.py
@@ -0,0 +1,11 @@
+from tweepy import Stream
+from tweepy import OAuthHander
+from tweepy.streaming import StreamListener
+
+access_token_key = "201313019-NBXXI5i97Kd0Efr9gwY3xCVV6c2xwbBS7VYu6P1a"
+access_token_secret = "w8LUQIqckxqbcdjb9L2E0OJ00aHD4ZVQogc6bGkJls46M"
+
+consumer_key = "TwhZ4ZDN6akZfa4eek5A"
+consumer_secret = "INSkPv6WJuADNTs1hv7D6REH8f0K9F07aSDxrgf1hw"
+
+
---
+++ twitterstream.py
@@ -0,0 +1,61 @@
+import oauth2 as oauth
+import urllib2 as urllib
+
+# Twitter credentials
+access_token_key = "201313019-NBXXI5i97Kd0Efr9gwY3xCVV6c2xwbBS7VYu6P1a"
+access_token_secret = "w8LUQIqckxqbcdjb9L2E0OJ00aHD4ZVQogc6bGkJls46M"
+
+consumer_key = "TwhZ4ZDN6akZfa4eek5A"
+consumer_secret = "INSkPv6WJuADNTs1hv7D6REH8f0K9F07aSDxrgf1hw"
+
+_debug = 0
+
+oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
+oauth_consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
+
+signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+
+http_method = "GET"
+
+
+http_handler  = urllib.HTTPHandler(debuglevel=_debug)
+https_handler = urllib.HTTPSHandler(debuglevel=_debug)
+
+'''
+Construct, sign, and open a twitter request
+using the hard-coded credentials above.
+'''
+def twitterreq(url, method, parameters):
+  req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                             token=oauth_token,
+                                             http_method=http_method,
+                                             http_url=url, 
+                                             parameters=parameters)
+
+  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+
+  headers = req.to_header()
+
+  if http_method == "POST":
+    encoded_post_data = req.to_postdata()
+  else:
+    encoded_post_data = None
+    url = req.to_url()
+
+  opener = urllib.OpenerDirector()
+  opener.add_handler(http_handler)
+  opener.add_handler(https_handler)
+
+  response = opener.open(url, encoded_post_data)
+
+  return response
+
+def fetchsamples():
+  url = "https://stream.twitter.com/1/statuses/sample.json"
+  parameters = []
+  response = twitterreq(url, "GET", parameters)
+  for line in response:
+    print line.strip()
+
+if __name__ == '__main__':
+  fetchsamples()
