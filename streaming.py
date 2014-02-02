8d6c34b2beeb07b0550f4229cf6679671fc80761
Santiago <santiago@semanttica.com>
2014-02-02 01:39:10 -0500

    changed authentication method

--- streaming.py
+++ streaming.py
@@ -4,9 +4,11 @@ from tweepy.streaming import StreamListener
 
 access_token_key = "201313019-NBXXI5i97Kd0Efr9gwY3xCVV6c2xwbBS7VYu6P1a"
 access_token_secret = "w8LUQIqckxqbcdjb9L2E0OJ00aHD4ZVQogc6bGkJls46M"
-
 consumer_key = "TwhZ4ZDN6akZfa4eek5A"
 consumer_secret = "INSkPv6WJuADNTs1hv7D6REH8f0K9F07aSDxrgf1hw"
+
+username = "stronnics"
+password = "S03752455666"
 
 class listener(StreamListener):
 
@@ -19,5 +21,9 @@ class listener(StreamListener):
 
 auth = OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token_key,access_token_secret)
-twitterStream = Stream(auth, listener())
+
+# Works with basic authentication, refuses auth
+twitterStream = Stream(username,password, listener())
 twitterStream.filter(track=["SuperBowl"])
+
+
