52152f85bece9a71445b0965b5d0015ac4498b8d
Santiago Paiva <paiva.santiago@gmail.com>
2014-02-03 08:31:13 -0500

    Updated steaming.py

--- streaming.py
+++ streaming.py
@@ -2,31 +2,37 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
-access_token_key = "201313019-NBXXI5i97Kd0Efr9gwY3xCVV6c2xwbBS7VYu6P1a"
-access_token_secret = "w8LUQIqckxqbcdjb9L2E0OJ00aHD4ZVQogc6bGkJls46M"
-consumer_key = "TwhZ4ZDN6akZfa4eek5A"
-consumer_secret = "INSkPv6WJuADNTs1hv7D6REH8f0K9F07aSDxrgf1hw"
-username = "stronnics"
-password = "S03752455666"
+access_token_key = "Insert code here"
+access_token_secret = "Insert code here"
+consumer_key = "Insert code here"
+consumer_secret = "Insert code here"
+username = "Insert username"
+password = "Insert password"
 
 class listener(StreamListener):
 
     def on_data(self, data):
-	try:
-        	#print data
+        try:
+        	
+            text = data.split(',"text":"')[1].split('","source')[0]
+            location = data.split(',"location":"')[1].split('","url')[0]
+            date = data.split('{"created_at":"')[1].split('","id')[0]
+            author = data.split(',"screen_name":"')[1].split('","location"')[0]
+            hashtags = data.split('"hashtags":[')[1].split('],"symbols')[0]   
 
-		tweet = data.split(',"text":"')[1].split('","source')[0]
-		
-		# Using ":::" to separate time and tweets
-		saveThis = str(time.time() + ":::" + tweet)
-		saveFile = open("twitDB2.csv", "a")
-		saveFile.write(saveThis)
-		saveFile.write("\n")
-		saveFile.close()
-        	return True
-	except BaseException, e:
-		print "faile on data", str(e)
-		time.sleep(5)
+	    # Tweet composed of date, author, location, text, and hashtags. 	
+            saveThis = str("{" + "Date:" + '"' + date + '",' + "Author:" + '"@' + author + '",' + "Location:" + '"' + location + '",'+ "Text:" + '[' + text + '],' + "Hashtags:" + "[" + hashtags + "]" + "},")
+ 	
+	    # Saves into a CVS file named "tweeter"	 	
+            saveFile = open("tweeter.csv", "a")
+            saveFile.write(saveThis)
+            saveFile.write("\n")
+            saveFile.close()
+            return True
+        
+        except BaseException, e:
+            print "failed on data", str(e)
+            time.sleep(5)
 
 
     def on_error(self, status):
@@ -35,8 +41,7 @@ class listener(StreamListener):
 auth = OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token_key,access_token_secret)
 
-# Works with basic authentication, refuses auth
-twitterStream = Stream(username,password, listener())
-twitterStream.filter(track=["SuperBowl"])
-
-
+#If script refuses auth authentication, use basic authentication instead (username and password)
+#twitterStream = Stream(username,password, listener())
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["SuperBowl,Super Bowl"])
