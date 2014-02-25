793d044da926bbf14069464c67f59326cdaf68bd
Santiago Paiva <paiva.santiago@gmail.com>
2014-02-25 13:31:07 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,6 +1,20 @@
 import csv
 import json
 
+csvfile  = open('superbowl-2.csv', "r")
+jsonfile  = open('out.json', "w")
+fieldnames = ("Date", "Author", "Location", "Text", "Hashtags")
 
+reader = csv.DictReader(csvfile,fieldnames)
+
+for row in reader:
+
+    json.dumps(row,jsonfile,indent=4)
+    jsonfile.write('\n')
+
+print(json.dumps(row,jsonfile,indent=4))
+
+csvfile.close()
+jsonfile.close()
 
 
